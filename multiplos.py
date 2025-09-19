917687c93e018af84d3c28e860b8abed1d7471fa
Baryhdz <bruce.hernandez@iteso.mx>
2025-09-18 21:16:35 -0600

    Refactor program header and improve input/output prompts for clarity

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,23 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Hernández Robles Bruce de Bary 
+18/09/2025
+
+El siguiente programa determina si dos números enteros son múltiplos uno del otro.
 """
 
 # Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+a = int(input("Ingresa el primer número entero"))
+b = int(input("Ingresa el segundo número entero"))
 
 # Proceso
+if a%b==0 or b%a==0:
+    print(f"{a} es un múltiplo de {b}")
+else:
+    print(f"Ninguno de los números es múltiplo del otro")
+
+
 
 
 # Salidas
-print(salida)
